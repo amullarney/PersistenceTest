1cc758a1a03f66fb474554468158d12bef8f2637
amullarney <alasdar@sbcglobal.net>
2021-05-28 17:00:31 -0700

    InitialSave
    All tests, except explicit referential values, pass when testewd with
    experimental Ciera 2.4.9

---
+++ PersistenceTest/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp $HOME/.m2/repository/io/ciera/runtime/2.4.9-SNAPSHOT/runtime-2.4.9-SNAPSHOT.jar:$HOME/.m2/repository/org/antlr/antlr4-runtime/4.9.1/antlr4-runtime-4.9.1.jar:$HOME/.m2/repository/org/xtuml/PersistTest/1.0.0-SNAPSHOT/PersistTest-1.0.0-SNAPSHOT.jar persistencetest.PersistenceTestApplication
